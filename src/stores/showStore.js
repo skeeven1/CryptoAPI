c5d87cc4c99f93ba2483db8f32da7712f4ad914a
TimotheCDGP <91454016+TimotheCDGP@users.noreply.github.com>
2024-06-07 02:30:35 +0200

    Update showStore.js
    ADDED Comparison + caching

--- src/stores/showStore.js
+++ src/stores/showStore.js
@@ -1,26 +1,46 @@
-import create from 'zustand'
-import axios from 'axios'
+import create from 'zustand';
+import axios from 'axios';
 
-const showStore = create((set) => ({
+const showStore = create((set, get) => ({
     res: {}, 
     query: '',
+    cache: {},
 
     setQuery: (e) => {
         showStore.getState().searchChartData();
     },
 
     searchChartData: async () => {
+        // We use cache there because we don't want to reach the API request limit in a few clicks !
+        const { cache } = get();
+        const cacheKey = 'bitcoin-max-daily';
+
+        if (cache[cacheKey]) {
+            set({ res: cache[cacheKey] });
+            return;
+        }
+        
         try {
             const res = await axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=max&interval=daily&precision=10`);
+            cache[cacheKey] = res; // Cache update
             set({ res });
         } catch (err) {
             console.error('searchChartData() failed the API request', err);
         }
     },
-
+    // This is for the dropdown list containing 'default' cryptos
     fetchChartData: async (id) => {
+        const { cache } = get();
+        const cacheKey = `${id}-100-daily`;
+
+        if (cache[cacheKey]) {
+            set({ res: cache[cacheKey] });
+            return;
+        }
+
         try {
             const res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=100&interval=daily&precision=10`);
+            cache[cacheKey] = res; // Cache update
             set({ res });
         } catch (err) {
             console.error('fetchChartData() failed the API request', err);
